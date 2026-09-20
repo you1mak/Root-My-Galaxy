676a842112e9be6658c9aa0c277c1738f8fabc9b
YouMak <youmakyahya@gmail.com>
2026-09-20 21:58:32 +0200

    Update API URLs in PayloadRepository

--- app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
+++ app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
@@ -137,9 +137,9 @@ class PayloadRepository(private val context: Context) {
 
     companion object {
         private const val COMMIT_API_URL =
-            "https://api.github.com/repos/BuSung-dev/Root-My-Galaxy-Payloads/git/ref/heads/main"
+            "https://api.github.com/repos/you1mak/Root-My-Galaxy-Payloads/git/ref/heads/main"
         private const val RAW_REPOSITORY =
-            "https://raw.githubusercontent.com/BuSung-dev/Root-My-Galaxy-Payloads"
+            "https://raw.githubusercontent.com/you1mak/Root-My-Galaxy-Payloads"
         private const val MUTABLE_RAW_PREFIX = "$RAW_REPOSITORY/main/"
         private const val MAX_COMMIT_RESPONSE_BYTES = 16 * 1024
         private const val MAX_MANIFEST_BYTES = 256 * 1024
